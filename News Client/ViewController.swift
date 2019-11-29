6791b947cc517cd8089d763ba9e3139390f9dd32
duycao2506 <duyckhl@gmail.com>
2019-11-30 01:48:45 +0800

    Initial Commit

---
+++ News Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News Client
+//
+//  Created by Duy Cao on 11/30/19.
+//  Copyright © 2019 Duy Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
